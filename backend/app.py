57179ecf5834f2c1a9403c7825d21d21fe7dacd7
ayu292906 <ayush99shewale@gmail.com>
2026-06-30 18:40:41 +0530

    Create uploads folder automatically

--- backend/app.py
+++ backend/app.py
@@ -34,6 +34,8 @@ db.init_app(app)
 UPLOAD_FOLDER = "uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 with app.app_context():
     db.create_all()
 
